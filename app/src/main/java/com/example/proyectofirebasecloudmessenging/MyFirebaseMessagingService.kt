a57d9aec235626eaa613c3863d70744611f2ef00
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2023-05-01 16:25:09 -0600

    Agrengando configuracion basica para el uso de firebase

---
+++ app/src/main/java/com/example/proyectofirebasecloudmessenging/MyFirebaseMessagingService.kt
@@ -0,0 +1,6 @@
+package com.example.proyectofirebasecloudmessenging
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+}
