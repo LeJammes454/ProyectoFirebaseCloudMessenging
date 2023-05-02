13ba04fe55a43830cad90f35040a271018c6e1ee
LeJammes454 <98853831+LeJammes454@users.noreply.github.com>
2023-05-01 19:47:43 -0600

    App cliente para el uso de Firebase Cloud Messaging

--- app/src/main/java/com/example/proyectofirebasecloudmessenging/MainActivity.kt
+++ app/src/main/java/com/example/proyectofirebasecloudmessenging/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.proyectofirebasecloudmessenging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM_TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+            // ...
+
+            val token = task.result
+            Log.d("FCM_TOKEN", "Token: $token")
+
+            // Muestra el token en un Toast
+
+            val msg = getString(R.string.token_fmt, token)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+        }
     }
 }
+
